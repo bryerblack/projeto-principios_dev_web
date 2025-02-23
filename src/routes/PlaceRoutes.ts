cd50178ff790925ce58954f4efc59ff3397c79bc
Ítalo Ramalho <italo.ramalho@ccc.ufcg.edu.br>
2025-02-23 12:30:00 -0300

    ajustes swagger

--- src/routes/PlaceRoutes.ts
+++ src/routes/PlaceRoutes.ts
@@ -4,28 +4,22 @@ import { PlaceController } from "../controllers/PlaceController";
 const router = Router();
 const placeController = new PlaceController();
 
-// 🔹 Criar um novo espaço
 router.post("/", placeController.createPlace.bind(placeController));
 
-// 🔹 Listar todos os espaços
 router.get("/", placeController.getAllPlaces.bind(placeController));
 
-// 🔹 Buscar espaço por ID
 router.get("/:id", (req, res) => {
   placeController.getPlaceById(req, res);
   return;
 });
 
-// 🔹 Deletar espaço por ID
 router.delete("/:id", placeController.deletePlace.bind(placeController));
 
-// 🔹 Adicionar equipamento a um espaço
 router.post(
   "/:place_id/equipments",
   placeController.addEquipmentToPlace.bind(placeController)
 );
 
-// 🔹 Remover equipamento de um espaço
 router.delete(
   "/:place_id/equipments/:equipmentId",
   placeController.removeEquipmentFromPlace.bind(placeController)
