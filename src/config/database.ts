10d56cbb5911ca963b8db0b94396a3c9bc5a2b8f
Ítalo Ramalho <italo.ramalho@ccc.ufcg.edu.br>
2025-03-17 16:49:00 -0300

    ajustes no swagger e validação de senha

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const sequelize = new Sequelize({
   dialect: 'mysql',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3307,
+  port: Number(process.env.DB_PORT) || 3306,
   username: process.env.DB_USER || 'admin',
   password: process.env.DB_PASS || 'admin123',
   database: process.env.DB_NAME || 'projeto_pweb',
